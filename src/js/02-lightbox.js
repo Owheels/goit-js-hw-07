c4fbd1f6589701f3098cd954f18746c667f183db
Oleksandr Kalienchenko <owheels.qa@gmail.com>
2023-05-31 11:00:11 +0300

    Task 02

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const container = document.querySelector(".gallery");
+const image = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+  )
+  .join("");
+container.insertAdjacentHTML("beforeend", image);
 
-console.log(galleryItems);
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
